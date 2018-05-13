892ba2f8a007f878e6b37f49dc97523d9f00320f
seanr89 <srafferty89@gmail.com>
2018-05-13 18:27:40 +0100

    Added button and basic switch object to view

--- app/src/main/java/com/example/seanrafferty/mygrouprandomiser/EventStatsActivity.kt
+++ app/src/main/java/com/example/seanrafferty/mygrouprandomiser/EventStatsActivity.kt
@@ -2,9 +2,11 @@ package com.example.seanrafferty.mygrouprandomiser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import com.example.seanrafferty.mygrouprandomiser.Business.EventManager
+import com.example.seanrafferty.mygrouprandomiser.Models.GroupEvent
 import com.example.seanrafferty.mygrouprandomiser.Models.Team
 import com.example.seanrafferty.mygrouprandomiser.Utilities.UtilityMethods
 
@@ -12,36 +14,54 @@ class EventStatsActivity : AppCompatActivity()
 {
 
     private var EventID = 0
+    private lateinit var Event : GroupEvent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_stats)
 
         EventID = intent.getStringExtra("EventID").toInt()
+        Event = RequestEventForEventID(EventID)
 
+        InitialiseNumberPickers()
+
+        var btn_update = findViewById<Button>(R.id.btnUpdateEvent)
+        btn_update.setOnClickListener()
+        {
+            UpdateEvent(Event)
+        }
+
+        RequestAndDisplayEventStats(Event)
+    }
+
+    private fun InitialiseNumberPickers() {
         var nbrPickerTeamOne = findViewById<NumberPicker>(R.id.pickerTeamOne)
         //Set the minimum value of NumberPicker
-        nbrPickerTeamOne.setMinValue(0);
+        nbrPickerTeamOne.minValue = 0;
         //Specify the maximum value/number of NumberPicker
-        nbrPickerTeamOne.setMaxValue(10);
+        nbrPickerTeamOne.maxValue = 10;
 
         var nbrPickerTeamTwo = findViewById<NumberPicker>(R.id.pickerTeamTwo)
         //Set the minimum value of NumberPicker
-        nbrPickerTeamTwo.setMinValue(0);
+        nbrPickerTeamTwo.minValue = 0;
         //Specify the maximum value/number of NumberPicker
-        nbrPickerTeamTwo.setMaxValue(10);
+        nbrPickerTeamTwo.maxValue = 10
+    }
 
-        RequestAndDisplayEventStats(EventID)
+    private fun RequestEventForEventID(eventID: Int) : GroupEvent
+    {
+        var eventManager = EventManager(this)
+        var event = eventManager.GetEventByID(EventID)
+
+        return event
     }
 
     /**
      * Request the relevant data for an event and display contents of screen
      * @param eventID : the unique ID of the event
      */
-    private fun RequestAndDisplayEventStats(eventID: Int)
+    private fun RequestAndDisplayEventStats(event: GroupEvent)
     {
-        var eventManager = EventManager(this)
-        var event = eventManager.GetEventByID(EventID)
 
         var txtEventDate = findViewById<TextView>(R.id.txtViewEventDateContent)
         txtEventDate.text = UtilityMethods.ConvertDateTimeToString(event.Date)
@@ -51,6 +71,11 @@ class EventStatsActivity : AppCompatActivity()
 
         var nbrPickerTeamTwo = findViewById<NumberPicker>(R.id.pickerTeamTwo)
         nbrPickerTeamTwo.value = event.EventTeams[1].Score
+    }
+
+    private fun UpdateEvent(event: GroupEvent)
+    {
+
     }
 
     /**
