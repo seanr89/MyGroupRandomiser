7de41c46c80427205eb895bfbb83fba97c6a4a85
seanr89 <srafferty89@gmail.com>
2018-05-18 23:15:31 +0100

    player insert call added

--- app/src/main/java/com/example/seanrafferty/mygrouprandomiser/SQLite/DataAccessorObjects/PlayerDao.kt
+++ app/src/main/java/com/example/seanrafferty/mygrouprandomiser/SQLite/DataAccessorObjects/PlayerDao.kt
@@ -1,7 +1,13 @@
 package com.example.seanrafferty.mygrouprandomiser.SQLite.DataAccessorObjects
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import com.example.seanrafferty.mygrouprandomiser.Models.Player
 
 @Dao
 interface PlayerDao {
+
+    @Insert(onConflict = REPLACE)
+    fun insert(player: Player)
 }
